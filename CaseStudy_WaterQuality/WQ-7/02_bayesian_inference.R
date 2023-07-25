d58a21d2248e696ab44cb4503eb54190baa43656
clsong <chuliangsong1994@gmail.com>
2023-07-25 17:05:53 -0400

    rename

--- CaseStudy_WaterQuality/WQ-7/02_bayesian_inference.R
+++ CaseStudy_WaterQuality/WQ-7/02_bayesian_inference.R
@@ -49,8 +49,35 @@ data_all <- site_all %>%
 forecast_df <- 1:2 %>% 
   map(~bayesian_inference(data_all[[.]], site_all[[.]]))
 
+saveRDS(forecast_df, 
+        file = here(folder, 'bayesian_prediction.rda'))
+
 forecast_df %>% 
   bind_rows() %>% 
   ggplot(aes(datetime, prediction)) +
-  geom_line() +
+  geom_line(aes(group = parameter), alpha = .3) +
   facet_wrap(~site_id)
+
+forecast_df %>% 
+  bind_rows() %>% 
+  ggplot(aes(datetime, prediction, color = site_id)) +
+  geom_smooth()
+
+forecast_df %>% 
+  bind_rows() %>% 
+  ggplot(aes(datetime, prediction, color = site_id)) +
+  geom_line(aes(group = interaction(parameter,site_id), 
+            alpha = .1))
+
+forecast_df %>% 
+  bind_rows() %>% 
+  group_by(datetime, site_id) %>% 
+  count(swimmable = prediction < 20) %>% 
+  mutate(prob_swimmable = n/sum(n)) %>% 
+  filter(swimmable) %>% 
+  select(datetime, site_id, prob_swimmable) %>% 
+  ggplot(aes(datetime, prob_swimmable, 
+             group = site_id,
+             color = site_id)) +
+  geom_line() 
+  
