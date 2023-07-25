cfdd58a05a56e92a89b71f9dc4d4756f301b645e
Irena Papst <papstirena@gmail.com>
2023-07-25 12:13:30 -0400

    Create code_review_example.R

---
+++ Lectures/code_review_example.R
@@ -0,0 +1,32 @@
+library(palmerpenguins)
+library(ggplot2)
+
+(penguins
+	%>% filter(island == "Torgersen")
+	%>% group_by(year, species) 
+	%>% summarise(count = n()) 
+	%>% ggplot(aes(x = year, y = count, color = species)) 
+	+ geom_line()
+	+ geom_point()
+	+ labs(title = "Species Count on Torgersen")
+)
+
+(penguins
+	%>% filter(island == "Biscoe")
+	%>% group_by(year, species) 
+	%>% summarise(count = n()) 
+	%>% ggplot(aes(x = year, y = count, color = species)) 
+	+ geom_line()
+	+ geom_point()
+	+ labs(title = "Species Count on Biscoe")
+)
+
+(penguins
+	%>% filter(island == "Dream")
+	%>% group_by(year, species) 
+	%>% summarise(count = n()) 
+	%>% ggplot(aes(x = year, y = count, color = species)) 
+	+ geom_line()
+	+ geom_point()
+	+ labs(title = "Species Count on Dream")
+)
