b2849a0377f780f7ab1bde1052de9236ff78a6d8
Jane Doe <jane@example.org>
2023-07-26 14:43:20 -0400

    Added baseline ricker model

--- CaseStudy_Fisheries/Team2/03a_baseline_ricker.R
+++ CaseStudy_Fisheries/Team2/03a_baseline_ricker.R
@@ -1,1 +1,8 @@
-# baseline ricker model
+# Runs the baseline ricker model
+# @baseline.mod is the model output
+
+#Define prediction year
+pred.year <- 2021
+
+#Run the baseline Ricker model
+baseline.mod <- RunModRetro(data, pred.year)
