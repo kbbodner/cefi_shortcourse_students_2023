d8233212f7cd16717ef669124d20997a3880c303
Mariana Peifer Bezerra <marianapeifer@gmail.com>
2023-07-26 13:52:21 -0400

    mariana2

--- CaseStudy_WaterQuality/WQ-7/test.R
+++ CaseStudy_WaterQuality/WQ-7/test.R
@@ -13,7 +13,7 @@ library(rjags)
 library(coda)
 
 
-targets <- read_csv('Data/targets-neon-chla.csv')
+targets <- read_csv('targets-neon-chla.csv')
 target_sites <- targets |> 
   distinct(site_id) |> 
   pull()
@@ -45,3 +45,42 @@ noaa_past <- arrow::open_dataset(s3_past) |>
                 datetime >= ymd('2017-01-01'),
                 variable %in% variables) |>
   collect()
+
+
+noaa_past_mean <- noaa_past |> 
+  mutate(datetime = as_date(datetime)) |> 
+  group_by(datetime, site_id, variable) |> 
+  summarize(prediction = mean(prediction, na.rm = TRUE), .groups = "drop") |> 
+  pivot_wider(names_from = variable, values_from = prediction) |> 
+  # convert air temp to C
+  mutate(air_temperature = air_temperature - 273.15) |> 
+  filter(datetime <= as_date(max(targets$datetime)))
+
+
+# generate forecast(s) for June 2023
+forecast_date <- as_date(max(targets$datetime))
+
+bucket <- paste0("neon4cast-drivers/noaa/gefs-v12/stage2/parquet/0/", forecast_date) 
+s3_future <- arrow::s3_bucket(bucket = bucket, endpoint_override = 'data.ecoforecast.org', anonymous = TRUE)
+
+variables <- c("air_temperature", "precipitation_flux")
+
+noaa_future <- arrow::open_dataset(s3_future) |> 
+  dplyr::filter(datetime >= forecast_date,
+                site_id %in% target_sites,
+                variable %in% variables) |> 
+  collect()
+
+write.csv(noaa_future, file = "noaa_future.csv")
+
+noaa_future_daily <- noaa_future |> 
+  mutate(datetime = as_date(datetime)) |> 
+  # mean daily forecasts at each site per ensemble
+  group_by(datetime, site_id, parameter, variable) |> 
+  summarize(prediction = mean(prediction)) |>
+  pivot_wider(names_from = variable, values_from = prediction) |>
+  # convert to Celsius
+  mutate(air_temperature = air_temperature - 273.15) |> 
+  select(datetime, site_id, air_temperature, parameter)
+
+noaa_future_daily[1:10,]
