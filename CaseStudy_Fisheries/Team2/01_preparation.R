78e52aca602daa1584ce7c742cd6b8989df1d1b0
Jane Doe <jane@example.org>
2023-07-26 14:31:03 -0400

    Commit1

--- CaseStudy_Fisheries/Team2/01_preparation.R
+++ CaseStudy_Fisheries/Team2/01_preparation.R
@@ -1,1 +1,23 @@
 # preparation, loading data, functions
+###Load libraries-------------
+library(rjags)
+library(coda)
+library(tidyverse)
+library(gsl) # need for lambert's W
+library(R2jags)
+library("rstudioapi")
+
+
+###Load functions-------------
+setwd(dirname(getActiveDocumentContext()$path))
+source("../Code/Functions.R")
+
+
+###Load data-------------
+data.raw <- read_csv("../DataIn/FraserSockeyeData2022.csv")
+
+#Filter on population
+selected.pop <- "Early Stuart"
+
+data <- data.raw |>
+  filter(Pop_Name == selected.pop)
