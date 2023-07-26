fe43525b4381740421d75ddd52ca3787a2f4ea6a
Jane Doe <jane@example.org>
2023-07-26 14:48:23 -0400

    Added some stuff on performance

--- CaseStudy_Fisheries/Team2/04_performance.R
+++ CaseStudy_Fisheries/Team2/04_performance.R
@@ -1,1 +1,21 @@
-# performance 
+# Script to determine the performance of various model predictions 
+#   according to different metrics
+#
+
+#function that carries out all the performance metrics for all functions in @param funs
+run.all.performances <- function(obs, pred, funs){
+  perf <- data.frame(metric=names(funs))
+  perf$performance <-  mapply(do.call, funs, list(list(obs, pred)))
+  perf
+}
+
+#Create a list of all the performance metrics we can use
+performance.functions <- list("MRE" = MRE, 
+                              "MAE" = MAE,
+                              "MPE" = MPE,
+                              "RMSE" = RMSE,
+                              "MAPE" = MAPE, 
+                              "MAAPE" = MAAPE,
+                              "MASE" = MASE)
+
+#TODO: implement performance on all models
