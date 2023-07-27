d62f769e1defdb335a97033ff4d847336b728992
Jane Doe <jane@example.org>
2023-07-27 16:37:28 -0400

    Ran Baseline for all years starting 1955

--- CaseStudy_Fisheries/Team2/03a_baseline_ricker.R
+++ CaseStudy_Fisheries/Team2/03a_baseline_ricker.R
@@ -1,8 +1,15 @@
 # Runs the baseline ricker model
 # @baseline.mod is the model output
 
-#Define prediction year
-pred.year <- 2021
-
 #Run the baseline Ricker model
-baseline.mod <- RunModRetro(data, pred.year)
+baseline.mod.all <- data.frame(matrix(ncol = 5, nrow = 0))
+colnames(baseline.mod.all) <- c("Pred_Year", "Mod", "ModType", "Pred4", "Pred5")
+
+#
+for(i in 8:nrow(data)) {
+  print(data$yr[i])
+  baseline.mod.all[nrow(baseline.mod.all) + 1,] <- RunModRetro(data, data$yr[i])$Preds_Out
+}
+
+baseline.mod.all <- baseline.mod.all |>
+  mutate(R_Pred=Pred4+Pred5)
