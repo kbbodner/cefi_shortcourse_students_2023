cfa265d40bd8cb4c8734d8babc1bf1e5c2a3d8f1
clsong <chuliangsong1994@gmail.com>
2023-07-27 16:08:54 -0400

    update data analysis and generate figures

--- CaseStudy_WaterQuality/WQ-7/03_data_analysis.R
+++ CaseStudy_WaterQuality/WQ-7/03_data_analysis.R
@@ -8,38 +8,82 @@ library(here)
 folder <- here("CaseStudy_WaterQuality", "WQ-7")
 load(here(folder, "data.RData"))
 
-source(here(folder,"toolbox.r"))
-load(here(folder,"bayesian_prediction_humid_air.rda"))
-# ggthemr::ggthemr("fresh")
+source(here(folder, "toolbox.r"))
 
 
- 
- forecast_df %>% 
-   bind_rows() %>% 
-   ggplot(aes(datetime, prediction)) +
-   geom_line(aes(group = parameter), alpha = .3) + geom_hline(yintercept = 20, color = "red", linetype = "dashed") +
-   facet_wrap(~site_id) 
- 
- forecast_df %>% 
-   bind_rows() %>% 
-   ggplot(aes(datetime, prediction, color = site_id)) +
-   geom_smooth()
- 
- forecast_df %>% 
-   bind_rows() %>% 
-   ggplot(aes(datetime, prediction, color = site_id)) +
-   geom_line(aes(group = interaction(parameter,site_id), 
-             alpha = .1))
- 
- forecast_df %>% 
-   bind_rows() %>% 
-   group_by(datetime, site_id) %>% 
-   count(swimmable = prediction < 20) %>% 
-   mutate(prob_swimmable = n/sum(n)) %>% 
-   filter(swimmable) %>% 
-   select(datetime, site_id, prob_swimmable) %>% 
-   ggplot(aes(datetime, prob_swimmable, 
-              group = site_id,
-              color = site_id)) +
-   geom_line() 
-   
+# combine predictions from different methods ------------------------------------------------------------
+load(here(folder, "bayesian_prediction.rda"))
+forecast_air <- forecast_df %>%
+  bind_rows()
+
+load(here(folder, "bayesian_prediction_m2.rda"))
+forecast_air_nonlinear <- forecast_df %>%
+  bind_rows()
+
+load(here(folder, "bayesian_prediction_humid_air.rda"))
+forecast_air_humid <- forecast_df %>%
+  bind_rows()
+
+forecast <- bind_rows(
+  forecast_air %>%
+    mutate(method = "~ Temp"),
+  forecast_air %>%
+    mutate(method = "~ Temp + Temp^2"),
+  forecast_air_humid %>%
+    mutate(method = "~ Temp + Humid"),
+)
+
+
+# plot results ------------------------------------------------------------
+ggthemr::ggthemr("fresh")
+
+forecast %>%
+  ggplot(aes(datetime, prediction, color = site_id)) +
+  geom_line(aes(group = interaction(site_id, parameter)), alpha = .05) +
+  geom_hline(yintercept = 20, color = "firebrick2", linetype = "dashed") +
+  facet_wrap(. ~ method, scales = "free") +
+  scale_color_manual(
+    values = c("#EFBB24", "#3A8FB7")
+  ) +
+  labs(
+    x = "Date",
+    y = "Prediction"
+  ) +
+  theme(
+    legend.title=element_blank(),
+    legend.position = 'bottom'
+  )
+ggsave(here(folder, 'raw_prediction.pdf'), width = 10, height = 5)
+
+forecast %>%
+  group_by(datetime, site_id, method) %>%
+  count(swimmable = prediction < 20) %>%
+  mutate(prob_swimmable = n / sum(n)) %>%
+  filter(swimmable) %>%
+  mutate(error = sqrt(prob_swimmable * (prob_swimmable)/ 500)) %>% 
+  select(datetime, site_id, prob_swimmable, method, error) %>%
+  ggplot(aes(datetime, prob_swimmable,
+    group = interaction(site_id, method),
+    color = site_id,
+    fill = site_id
+  )) +
+  geom_line(linewidth = 1) +
+  # geom_ribbon(aes(ymin = prob_swimmable - error,
+  #                 ymax = prob_swimmable + error), alpha = .2) +
+  scale_color_manual(
+    values = c("#EFBB24", "#3A8FB7")
+  ) +
+  # scale_fill_manual(
+  #   values = c("#EFBB24", "#3A8FB7")
+  # ) +
+  facet_wrap(~method) +
+  labs(
+    y = 'Probability of swimmiable',
+    x = 'Date'
+  ) +
+  theme(
+    legend.title=element_blank(),
+    legend.position = c(.2, .2)
+  )
+ggsave(here(folder, 'swimmable_prob.pdf'), width = 10, height = 5)
+
