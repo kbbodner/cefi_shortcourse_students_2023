779d79a071e0a6e0a97112cf44822c99a6344e5a
jackagoldman <jandrewgoldman@gmail.com>
2023-07-28 10:31:56 -0400

    update readme and power uncertainty

---
+++ CaseStudy_Fisheries/Team2/05_power_uncertainty.R
@@ -0,0 +1,94 @@
+# unclean - power forecast
+
+Dat = data
+
+
+# for year X forecast, would have return data up to year X-1
+# age-4 return (rec4) up to brood year X-5, age-5 return (rec5) up to brood year X-6
+
+Data_Retro <- Dat %>% filter(yr <= (Pred_Year-4))
+Data_Retro[Data_Retro$yr == (Pred_Year-5), c("rec5", "R")] <- NA
+Data_Retro[Data_Retro$yr == (Pred_Year-4), c("rec4", "rec5", "R")] <- NA
+
+# Want to scale down obs to make models more stable
+# only really required for TMB, but will use for all types
+Scale <- 10^(floor(log(mean(Data_Retro$R, na.rm=T), 10))) # most numbers should be below 10
+
+# Set up data and starting value lists list to go into model
+data2 <- list()
+data2$R_Obs <- Data_Retro$R/Scale
+data2$N <- dim(Data_Retro)[1]
+
+data2$A_mean <- 1
+data2$A_tau <- 1
+data2$B_mean <- 1
+data2$B_tau <- 1
+data2$Sig_Gam_Dist <- 0.001
+data2$S <- Data_Retro$S/Scale 
+
+inits <- function(){
+  param <- list()
+  param$tau <- runif(1, 0.001, 10)
+  param$A <- rnorm(1, 1, 0.5)
+  param$B <- rnorm(1, 1, 0.5)
+  param
+}
+
+power <- "
+model{
+A ~ dnorm(A_mean, A_tau)             # prior for alpha
+B ~ dnorm(B_mean, B_tau)                # prior for beta
+tau ~ dgamma(Sig_Gam_Dist,Sig_Gam_Dist)    # prior for precision parameter
+sigma <- 1/sqrt(tau)  
+
+for (i in 1:N) {                             # loop over N sample points
+      R_Obs[i] ~ dlnorm(logR_Fit[i], tau)          # likelihood -> predicted value for NA in data set
+      logR_Fit[i] <- A + B * log(S[i])       # power model
+      R_Fit[i] <- exp(logR_Fit[i])
+      R_Pred[i] ~ dlnorm(logR_Fit[i],tau)
+}
+}
+"
+
+
+
+j.model2   <- jags.model(file = textConnection(power),
+                         data = data2,
+                         inits = list(inits(), inits(), inits()),
+                         n.chains = 3)
+
+var.out2 <- coda.samples (model = j.model2,
+                          variable.names = c("A", "B","tau"),
+                          n.iter = 2000)
+
+## remember to assess convergence and remove burn-in before doing other diagnostics
+GBR <- gelman.plot(var.out2)
+
+## convert to matrix
+var.mat2      <- as.matrix(var.out2)
+
+## Pairwise scatter plots & correlation
+pairs(var.mat2)	## pairs plot to evaluate parameter correlation
+cor(var.mat2)  
+
+nsamp <- 5000
+samp <- sample.int(nrow(var.mat2),nsamp)
+xpred <- 2022 					## sequence of x values we're going to
+npred <- length(xpred)				##      make predictions for
+ypred <- matrix(0.0,nrow=nsamp,ncol=npred)	## storage for predictive interval
+ycred <- matrix(0.0,nrow=nsamp,ncol=npred)	## storage for credible interval
+
+
+for(g in seq_len(nsamp)){
+  theta = var.mat2[samp[g],]
+  ycred[g,] <- theta["A"] + theta["B"]*xpred
+  ypred[g,] <- rnorm(npred,ycred[g,],1/sqrt(theta["tau"]))
+}
+
+ci <- apply(ycred,2,quantile,c(0.025,0.5,0.975))  ## credible interval and median
+pi <- apply(ypred,2,quantile,c(0.025,0.975))
+
+j.model2$R_Pred
+summary(var.out2)$statistics
+plot(Year,  )
+
