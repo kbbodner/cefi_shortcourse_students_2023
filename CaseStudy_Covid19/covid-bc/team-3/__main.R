bdf7ca2f6f038db2d04f8bdf6df25420c89ea364
Cole Brookson <cole.brookson@gmail.com>
2023-09-09 14:53:58 -0400

    fixing student code

---
+++ CaseStudy_Covid19/covid-bc/team-3/__main.R
@@ -0,0 +1,4 @@
+#' This main file acts to run all of the code for this particular case study
+#' in one place as a form of "reproducible" workflow
+
+# run original stan case study =================================================
